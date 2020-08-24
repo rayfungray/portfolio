c6f1f3818329582c080ac5323bcbe1022728e137
rayfungray <61134260+rayfungray@users.noreply.github.com>
2020-08-24 00:56:03 -0600

    fix form

--- action_page.php
+++ action_page.php
@@ -9,10 +9,9 @@ if(isset($_POST['submit'])){
     $message = $name . " " . " wrote the following:" . "\n\n" . $_POST['message'];
     $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
 
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    
+    mail($to,$subject,$message);
+    mail($from,$subject2,$message2); // sends a copy of the message to the sender
     echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     // You cannot use header and echo together. It's one or the other.
